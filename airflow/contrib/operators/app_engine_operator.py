4e254ee9dd5bc48948313238118f62b761123b4d
Alexa Griffith <agriffith@bluecore.com>
2020-04-10 12:48:52 -0400

    changing template fields to be a tupe where the second elem is empty

--- airflow/contrib/operators/app_engine_operator.py
+++ airflow/contrib/operators/app_engine_operator.py
@@ -117,7 +117,7 @@ class AppEngineOperatorSync(BaseOperator):
     :param kwargs: Named parameters to pass to BaseOperator constructor
     :type kwargs: dict
     """
-    template_fields = ('command_name')
+    template_fields = ('command_name',)
 
     @apply_defaults
     def __init__(self,
@@ -208,7 +208,7 @@ class AppEngineOperatorAsync(BaseOperator):
     :param kwargs: Named parameters to pass to BaseOperator constructor
     :type kwargs: dict
     """
-    template_fields = ('command_name')
+    template_fields = ('command_name',)
 
     @apply_defaults
     def __init__(self,
